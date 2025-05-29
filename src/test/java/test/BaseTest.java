63caafc97080cc8a7a9b9842cf507e04b8f2d47a
AlexandruChilom <alexchilom30@gmail.com>
2025-05-29 17:21:02 +0200

    Added mobile view tests

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,29 +1,39 @@
 package test;
 
+import annotations.MobileTest;
 import data.Constants;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BaseTest {
     public WebDriver driver;
+
     @BeforeMethod
-    public void setup() {
-        driver = new FirefoxDriver();
+    public void setup(Method method) {
+        boolean isMobile = method.isAnnotationPresent(MobileTest.class);
+        ChromeOptions options = new ChromeOptions();
+        if (isMobile){
+            options = setMobileView();
+        }
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(Constants.URL);
-//        Wait.waitInSeconds(3);
     }
 
-    @Test
-    public void goToWebsiteTest() {
-        // Pentru a deschide un browser folosim obiectul WebDriver din Selenium.
-
-        // Pentru a naviga pe un site folosim metoda .get pe obiectul driver
+    private ChromeOptions setMobileView(){
+        Map<String,String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName","Samsung Galaxy S20 Ultra");
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("mobileEmulation",mobileEmulation);
+        return options;
     }
-
 }
--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import annotations.MobileTest;
 import data.Constants;
 
 import org.testng.Assert;
@@ -8,10 +9,26 @@ import org.testng.annotations.Test;
 import page.objects.HomePage;
 import page.objects.LoginPage;
 
+
 public class LoginTest extends BaseTest {
 
     @Test
     public void loginTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnDisagreeButton();
+        homePage.clickOnLoginLink();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendEmailAddress();
+        loginPage.sendPassword();
+        loginPage.clickOnSignInButton();
+        String actualMessage = loginPage.successfulLoginConfirmation();
+        String expectedMessage = Constants.LOGIN_CONFIRMATION;
+        Assert.assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    @MobileTest
+    public void mobileLoginTest(){
         HomePage homePage = new HomePage(driver);
         homePage.clickOnDisagreeButton();
         homePage.clickOnLoginLink();
